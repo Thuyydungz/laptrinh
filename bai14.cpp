18770f16e3751b7431b28d9db4d4c16a4258a957
Thuyydungz <144414433+Thuyydungz@users.noreply.github.com>
2023-09-08 12:26:16 +0700

    Add files via upload

---
+++ bai14.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main14()
+{
+	float ten , diachi , sdt , chuyennganh;
+	ten = Le Thuy Dung;
+	diachi = Phuc Yen_Vinh Phuc_280000;
+	sdt = 0346284721;
+	chuyennganh = cntt;
+	cout<<"thong tin ca nhan"<<' '<<ten , diachi , sdt , chuyennganh;
+	return 0;
+}
